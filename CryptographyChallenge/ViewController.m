52d8c2a6cedb1944c2863270cec587be0e284256
mjvoracek <mjvoracek@gmail.com>
2014-04-03 22:20:07 -0500

    initial commit

--- CryptographyChallenge/ViewController.m
+++ CryptographyChallenge/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+NSString *const inputString = @"Hello camrade, I bring news from Oceania. We will soon feast on kangaroo meat. Their defences are weak and I can't spell \"defense\". Keep hope alive and aviod the grue.";
+NSString *const successString = @"Uryyb pnzenqr, V oevat arjf sebz Bprnavn. Jr jvyy fbba srnfg ba xnatnebb zrng. Gurve qrsraprf ner jrnx naq V pna'g fcryy \"qrsrafr\". Xrrc ubcr nyvir naq nivbq gur tehr.";
+NSString *const uppercaseAlphabet = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+NSString *const lowercaseAlphabet = @"abcdefghijklmnopqrstuvwxyz";
+
 @interface ViewController ()
 
 @end
@@ -17,13 +22,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *output = [self encodeString:inputString withOffset:13];
+    
+    NSLog(@"%@", output);
+    if ([output isEqualToString:successString])
+    {
+        NSLog(@"Successful cypher! Congratulations.");
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning
+-(NSString *)encodeString:(NSString *)aString withOffset:(NSUInteger)offset
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSMutableString *finishedString = [[NSMutableString alloc] init];
+    NSString *charString;
+    char character;
+    
+    for (int i = 0; i < [aString length]; i++)
+    {
+        character = [aString characterAtIndex:i];
+        charString = [NSString stringWithFormat:@"%c",character];
+        NSRange upperRange = [uppercaseAlphabet rangeOfString:charString];
+        NSRange lowerRange = [lowercaseAlphabet rangeOfString:charString];
+        
+        if (upperRange.location != NSNotFound)
+        {
+            if ((upperRange.location + offset) >= [uppercaseAlphabet length])
+            {
+                NSRange shift = NSMakeRange ((upperRange.location+offset)%[uppercaseAlphabet length], 1);
+                character = [uppercaseAlphabet characterAtIndex:shift.location];
+                charString = [NSString stringWithFormat:@"%c",character];
+            }
+            else
+            {
+                NSRange shift = NSMakeRange ((upperRange.location+offset), 1);
+                character = [uppercaseAlphabet characterAtIndex:shift.location];
+                charString = [NSString stringWithFormat:@"%c",character];
+            }
+        }
+        
+        if (lowerRange.location != NSNotFound)
+        {
+            if ((lowerRange.location + offset) >= [lowercaseAlphabet length])
+            {
+                NSRange shift = NSMakeRange ((lowerRange.location+offset)%[lowercaseAlphabet length], 1);
+                character = [lowercaseAlphabet characterAtIndex:shift.location];
+                charString = [NSString stringWithFormat:@"%c",character];
+            }
+            else
+            {
+                NSRange shift = NSMakeRange ((lowerRange.location+offset), 1);
+                character = [lowercaseAlphabet characterAtIndex:shift.location];
+                charString = [NSString stringWithFormat:@"%c",character];
+            }
+            
+        }
+        
+        [finishedString appendString:charString];
+    }
+    return finishedString;
 }
+
+
 
 @end
